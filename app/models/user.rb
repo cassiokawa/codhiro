192667598263794c2e6ba0ed70eebe569d137d39
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-07-24 19:10:05 -0300

    Hope everything, except omni are working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
-
+         :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, length: {maximum: 25}
 
   after_create :send_notification
@@ -12,83 +11,7 @@ class User < ActiveRecord::Base
       MyMailer.new_user(self).deliver
   end
 
-  def self.from_google_oauth2(access_token, signed_in_resource=nil)
-  	data = access_token.info
-  	user = User.where(:provider => access_token.provider, :uid => access_token.uid).first
-  	
-  	if user
-  		return user
-  	else
-  		registered_user = User.where(:email => access_token.email).first
-  		if registered_user
-  			return registered_user
-  		else
-  			user = User.create(
-  					name:data["name"],
-  					provider: access_token.provider,
-  					email: data["email"],
-  					uid: access_token.uid,
-  					image: data["image"],
-  					password: Devise.friendly_token[0,20]
-  				)
-  		end
-  	end
-  end
-
-
-  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-  	data = access_token.info
-  	user = User.where(:provider => access_token.provider, :uid => access_token.uid).first
-  	
-  	if user
-  		return user
-  	else
-  		registered_user = User.where(:email => data.email).first
-  		if registered_user
-  			return registered_user
-  		else
-  			user = User.create(
-  					name: access_token.extra.raw_info.name,
-  					provider: access_token.provider,
-  					email: data.email,
-  					uid: access_token.uid,
-  					image: data.image,
-  					password: Devise.friendly_token[0,20]
-  				)
-  		end
-  	end
-  end
-
-
-  def self.find_for_github_oauth(access_token, signed_in_resource=nil)
-  	data = access_token["info"]
-  	user = User.where(:provider => access_token["provider"], :uid => access_token["uid"]).first
-  	
-  	if user
-  		return user
-  	else
-  		registered_user = User.where(:email => data["email"]).first
-  		if registered_user
-  			return registered_user
-  		else
-				if data["name"].nil?
-						name = data["nickname"]
-					else
-						name = data["name"]
-					end
-
-
-  			user = User.create(
-  					name: name,
-  					provider: access_token["provider"],
-  					email: data["email"],
-  					uid: access_token["uid"],
-  					image: database_authenticatable["image"],
-  					password: Devise.friendly_token[0,20]
-  				)
-  		end
-  	end
-  end
+  
 
 
 
