b6bfea8f65ec33e6934df3d930c22e483d97549c
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-08-03 16:30:16 -0300

    Rating with number works, AVG rating too

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,12 @@
 class Project < ActiveRecord::Base
 	extend FriendlyId
-  friendly_id :name, use: [:slugged, :finders]
+    friendly_id :name, use: [:slugged, :finders]
 	has_many :tasks
 
 	has_many :subscriptions
 	has_many :users, through: :subscriptions
+
+	has_many :reviews
 
 	validates :name , presence: true, length: {maximum: 50}
 	validates :content , presence: true, length: {maximum: 500}
@@ -16,5 +18,10 @@ class Project < ActiveRecord::Base
   def shortname
   	name.length > 25? name[0..25]+"..." : name
   end
+
+  def average_rating
+  	reviews.blank? ? 0 : reviews.average(:star)
+  end
+
 
 end
