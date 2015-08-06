00a11f9cb02aaf03f9f88fb27dbaa7b1068effab
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-08-06 17:36:46 -0300

    added script to precompile active admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.precompile += %w[active_admin.css active_admin.js]
-  
+    
   config.action_mailer.default_url_options = {host: 'https://projsnova.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
