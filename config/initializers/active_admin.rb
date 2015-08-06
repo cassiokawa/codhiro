56770e99bc0b623d2fb1944425eacb8b3f77d374
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-08-06 16:54:45 -0300

    Still trying to solve activeadmin problme

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Codehero"
+  config.site_title = "Projeto Supernova"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
