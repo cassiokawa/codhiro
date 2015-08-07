cd11fe7e23812e2ef844c6d8fea90f67c2038f1f
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-08-07 14:42:33 -0300

    Added Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,13 @@ Rails.application.configure do
 
   config.assets.initialize_on_precompile = false
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 
 end
