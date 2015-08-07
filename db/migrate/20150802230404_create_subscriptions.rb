fa6581829236d62136e6e1bb0187f71ad68700a3
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-08-07 13:22:23 -0300

    Fingers crossed

--- db/migrate/20150802230404_create_subscriptions.rb
+++ db/migrate/20150802230404_create_subscriptions.rb
@@ -6,11 +6,6 @@ class CreateSubscriptions < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-
-    add_foreign_key :subscriptions, :projects
-    add_foreign_key :subscriptions, :users
-
     add_index :subscriptions, [:project_id, :user_id], :unique => true
-
   end
 end
